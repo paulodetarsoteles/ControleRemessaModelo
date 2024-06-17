9cac7b2a72f3258beaef9d13c77e68c0315b0c9e
Paulo <paulodetarso.teles@hotmail.com>
2024-06-17 18:49:07 -0300

    Criação dos projetos de Negocio, Repositorio e Entidades. Criação também da controller Home com o endpoint de teste.

---
+++ backend/ControleRemessaModelo/ControleRemessaModelo.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleRemessaModelo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Api funcionando.");
+        }
+    }
+}
